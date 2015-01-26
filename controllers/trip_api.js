6dbdfb8734408181c6a7e80bf43e5d92158a8742
squidgetx <squidgetx@gmail.com>
2015-01-26 14:11:05 -0500

    route reorganization

---
+++ controllers/trip_api.js
@@ -0,0 +1,29 @@
+var data = require('../models/trip');
+
+var controller = {};
+
+controller.get_by_id = function(req, res, next) {
+  data.Trip.forge({id: req.params.id})
+    .fetch()
+    .then(function(trip) {
+      if (trip) {
+        res.json({error: false,
+                  data: trip.toJSON()
+        });
+      } else {
+        res.status(500).json({error: true,
+          data: 'Record not found'})
+      }
+    })
+    .otherwise(function(error) {
+      res.status(500).json({error: true,
+                            data: error.message
+      });
+    });
+};
+
+module.exports = controller;
+
+
+
+
