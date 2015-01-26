e8173177960477a23d085a754c236279ba7b62c6
squidgetx <squidgetx@gmail.com>
2015-01-25 21:26:15 -0500

    other routes for trips

--- routes/trips.js
+++ routes/trips.js
@@ -7,5 +7,13 @@ router.get('/', function(req, res, next) {
   res.json(trips.get_trip(req.query.id));
 });
 
+router.post('/new', function(req, res, next) {
+  res.send('new request received');
+});
+
+router.put('/signup', function(req, res, next) {
+  //trips.signup(req.query.id, current_user);
+  res.send('signup request received');
+});
 
 module.exports = router;
