4de4b28b2822a12a61d75f999c610c6f605018ed
squidgetx <squidgetx@gmail.com>
2015-02-10 21:10:33 -0500

    initial commit

--- models/index.js
+++
@@ -1,9 +0,0 @@
-var trip = require('./trip');
-var user = require('./user');
-var _ = require('lodash');
-
-var data = {}
-data = _.merge(data, trip);
-data = _.merge(data, user);
-
-module.exports = data
