848f9fb6ecf1302ddcb9e7c341ae3205c6a42d70
squidgetx <squidgetx@gmail.com>
2015-08-21 12:41:55 -0400

    remove authentication for now

--- app/controllers/trips_controller.rb
+++ app/controllers/trips_controller.rb
@@ -1,11 +1,6 @@
 class TripsController < ActionController::Base
 
   def show
-    binding.pry
-    unless request.session['cas']
-      render json: nil, status: :unauthorized
-      return
-    end
     @trip = Trip.find(params[:id]).to_json
     render json: @trip
   end
